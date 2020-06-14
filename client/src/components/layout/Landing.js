03ef7c7599269e31e28a28fbbe3c27d9f73c0d0e
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2020-06-14 22:59:39 +0200

    DDC logo

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -13,7 +13,7 @@ export const Landing = (props) => {
             <>
             <div className="text text-center text-white display-4 font-weight-bold">DCI Developer Community</div>
             <p className="p-text text-center text-white mt-3">Create a developer portfolio, share posts and get help from other developers</p>
-            <img  className="logoDD" src="../../DDC-8.png"/>
+            <img  className="logoDD" src="../../DDC2-8.png"/>
             <div className="card">
                 <Form className="login-form">
                     <h1 className="text-center font-weight-bolder ">
