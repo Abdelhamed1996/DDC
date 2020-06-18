1dea2159013eada38b3cb9f31e1ec353d9a283ef
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2020-06-18 14:19:03 +0200

    Register & Login

--- client/src/components/layout/Register.js
+++ client/src/components/layout/Register.js
@@ -1,33 +1,76 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import{Form,Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 
 
 
 
-export const Register = (props) => {
+const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
 
+    const { name, email, password, password2 } = formData;
+
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            console.log(formData);
+        }
+    };
 
     return (
         <>
-            <Form className=" Form1 p-3 mt-5 respnsiv_form2  respnsiv_form3 rounded ">
+            <Form className=" Form1 p-3 mt-5 respnsiv_form2  respnsiv_form3 rounded " onSubmit={onSubmit}>
                 <h1 className="text-center mar-5hv">Sign Up</h1>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Name" />
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Enter Name" 
+                    name="name"                             
+                    value={name}
+                    onChange={onChange}
+                    />
                 </Form.Group>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control 
+                    type="email" 
+                    placeholder="Enter email"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                     />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                    type="password" 
+                    placeholder="Password" 
+                    name="password"
+                    value={password}
+                    onChange={onChange} 
+                    />
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder=" Confirm Password" />
+                    <Form.Control 
+                    type="password" 
+                    placeholder=" Confirm Password"
+                    name="password2"
+                    value={password2}
+                    onChange={onChange}
+                    />
                 </Form.Group>
                 <Form.Group controlId="formBasicCheckbox">
                     <Link to="/login">do you have allready an account?</Link>
