58f3c6aaa9b1ccd96780f14edf24b370bece92c2
arekdci <arekciura85@gmail.com>
2020-07-03 21:46:25 +0200

    c

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import Src from '../../images/Ethnic friendship-rafiki.svg'
 
 export const Landing = (props) => {
 
-    if(props.isAuht){
+    if (props.isAuht) {
         props.history.push('/dashboard')
-  }
+    }
     return (
         <>
             <div className="text text-center text-white display-4 font-weight-bold">DCI Developer Community</div>
             <p className="p-text text-center text-white mt-3 mar-5hv">Create a developer portfolio, share posts and get help from other developers</p>
 
-            <img src={Src} className="LP_I"/>
+            <img src={Src} className="LP_I" />
             <div className="text text-center text-white display-4 font-weight-bold">600+ Students</div>
         </>
     );
 }
-const mapStateToProps= state=>({
+const mapStateToProps = state => ({
     isAuht: state.auth.isAuht
 })
 
