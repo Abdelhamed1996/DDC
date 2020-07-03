db27b24324dc9ebaa13ac1d2e129a1d1e62dcc53
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2020-07-03 21:34:29 +0200

    kleinigkeiten

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import Src from '../../images/Ethnic friendship-rafiki.svg'
+import Src from '../../images/Artboard 1.svg'
 
 export const Landing = (props) => {
 
@@ -11,7 +11,6 @@ export const Landing = (props) => {
         <>
             <div className="text text-center text-white display-4 font-weight-bold">DCI Developer Community</div>
             <p className="p-text text-center text-white mt-3 mar-5hv">Create a developer portfolio, share posts and get help from other developers</p>
-
             <img src={Src} className="LP_I"/>
             <div className="text text-center text-white display-4 font-weight-bold">600+ Students</div>
         </>
