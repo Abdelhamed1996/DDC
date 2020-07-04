26ceb269b448d277feeda0f44a57547947cc7862
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2020-07-04 16:53:07 +0200

    Landing Page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import Src from '../../images/Artboard 1.svg'
+import Src from '../../images/Students.svg'
 
 export const Landing = (props) => {
 
