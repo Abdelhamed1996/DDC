c07a9e8985356416e2c3707368730be6f68a5026
arekdci <arekciura85@gmail.com>
2020-07-12 13:50:48 +0200

    Post responsiv

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -17,30 +17,30 @@ const PostItem = ({
     post: { _id, text, name, avatar, user, likes, comments, date }
 }) =>
     <>
-        <Container className="post-container">
-            <Row className="posts d-flex justify-content-center align-items-center m-1">
-                <div className="posts-head p-4">
-                    <Card.Img src={Img} className="head_size img-fluid" alt="avatar" />
+        <Container className="post-container ">
+            <Row className="posts d-flex justify-content-center align-items-center ">
+                <div className="posts-head">
+                    <Card.Img src={Img} className="head_size img-fluid post" alt="avatar" />
                     <Card.Body className="text-b">
                         <Card.Title className="posts-title d-flex justify-content-center">{name}</Card.Title>
                     </Card.Body>
                 </div>
-                <Col>
-                    <Card.Body className="posts-content">
+                <Col className="post-h">
+                    <Card.Body className="posts-content mt-4">
 
-                        <Card.Text>
+                        <Card.Text className="txt-f d-flex flex-column">
                             {text}
                         </Card.Text>
-                        
-                        <p><Moment format='YYYY/MM/DD'>{date}</Moment></p>
-                        <div className="posts-bottom d-flex justify-content-end align-items-center">
-                            <div onClick={e => addLike(_id)} className="postBtn"><i class="far fa-thumbs-up d m-3" ></i></div><span>{likes.length}</span>
-                            <div onClick={e => removeLike(_id)} className="postBtn"><i class="far fa-thumbs-down d m-3 "></i></div>
-                            {!auth.loading && user === auth.user._id && (
-                                <Button onClick={e => deletePost(_id)} variant="danger d  m-2">Delete</Button>
-                            )}
+                        <div className="content-b d-flex justify-content-between align-items-end">
+                            <p className="date-m"><Moment format='YYYY/MM/DD'>{date}</Moment></p>
+                            <div className="posts-bottom d-flex justify-content-end align-items-baseline">
+                                <div onClick={e => addLike(_id)} className="postBtn"><i class="far fa-thumbs-up d m-3" ></i></div><span>{likes.length}</span>
+                                <div onClick={e => removeLike(_id)} className="postBtn"><i class="far fa-thumbs-down d m-3 "></i></div>
+                                {!auth.loading && user === auth.user._id && (
+                                    <Button onClick={e => deletePost(_id)} variant="danger d  m-2">Delete</Button>
+                                )}
+                            </div>
                         </div>
-                        
 
                     </Card.Body>
                 </Col>
