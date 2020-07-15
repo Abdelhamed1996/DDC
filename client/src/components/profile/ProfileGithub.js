dd4d2c60c912d35838b35fdfc3088248dee6c7ce
arekdci <arekciura85@gmail.com>
2020-07-15 12:26:21 +0200

    DesignX

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -10,7 +10,7 @@ const ProfileGithub=({username, getGithubRepos, repos})=>{
     },[getGithubRepos(username)])
     return <>
             <div className="profile-github">
-                <h2 className="text-primary my-1">Github Repos</h2>
+                <h2 className="text-primary my-1 text-center mt-4">Github Repos</h2>
                 {repos.map(repo => (
                     <div key={repo.id} className="repo bg-white p-1 my-1">
                       <div>
